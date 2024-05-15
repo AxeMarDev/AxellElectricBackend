afe8fa2dd29a4075e95483828c5acb63ff5ea1c8
Axell Martinez Avila <axell.martinezavila@gmail.com>
2024-05-15 00:00:04 -0500

    Added add project route and added sequence to id key on project table

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
 	"os"
+	"time"
 )
+
+type Project struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	Data     string `json:"date"`
+	Imageurl string `json:"imageurl"`
+}
 
 var db *sql.DB
 
@@ -41,9 +53,74 @@ func initDB() {
 	}
 }
 
+func postProject(c *gin.Context) {
+	var newProject Project
+
+	// Bind the received JSON to newPerson
+	if err := c.ShouldBindJSON(&newProject); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Insert newPerson into the database
+	query := `INSERT INTO projects (name, location, imageurl) VALUES ($1, $2, $3) RETURNING id`
+	var id int
+	err := db.QueryRow(query, newProject.Name, newProject.Location, newProject.Imageurl).Scan(&id)
+
+	if err != nil {
+		log.Printf("Error while inserting new person: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add new person"})
+		return
+	}
+
+	// Return the new person as JSON
+	c.JSON(http.StatusCreated, newProject)
+}
+
+func getPeople(c *gin.Context) {
+	rows, err := db.Query("SELECT id, name, location, imageurl FROM projects")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to query people"})
+		return
+	}
+	defer rows.Close()
+
+	var projects []Project
+	for rows.Next() {
+		var p Project
+		if err := rows.Scan(&p.ID, &p.Name, &p.Location, &p.Imageurl); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan person"})
+			return
+		}
+		projects = append(projects, p)
+	}
+	fmt.Println(projects)
+
+	c.IndentedJSON(http.StatusOK, projects)
+}
+
 func main() {
 
 	initDB()
+	defer db.Close()
 
-	fmt.Println("hello world")
+	router := gin.Default()
+
+	// middleware
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.GET("/projects", getPeople)
+	router.POST("/projects", postProject)
+	err := router.Run("localhost:8080")
+
+	if err != nil {
+		return
+	}
 }
