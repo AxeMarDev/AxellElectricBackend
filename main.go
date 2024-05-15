2a49578b878d322bdf6e909864f83ca706c44556
Axell Martinez Avila <axell.martinezavila@gmail.com>
2024-05-15 01:41:36 -0500

    added project delete route

--- main.go
+++ main.go
@@ -99,6 +99,42 @@ func getPeople(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, projects)
 }
 
+func deleteProject(c *gin.Context) {
+	// Get the id from the URL parameter
+	id := c.Query("id") // This method is used for query parameters
+	fmt.Println(id)
+
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing ID in the URL parameter"})
+		return
+	}
+
+	// Execute the delete query
+	query := `DELETE FROM projects WHERE id = $1`
+	result, err := db.Exec(query, id)
+	if err != nil {
+		log.Printf("Error while deleting person: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete person"})
+		return
+	}
+
+	// Check how many rows were affected
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("Error getting rows affected: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error checking deletion result"})
+		return
+	}
+
+	if rowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "No project found with the provided ID"})
+		return
+	}
+
+	// Return success message
+	c.JSON(http.StatusOK, gin.H{"message": "Person deleted successfully"})
+}
+
 func main() {
 
 	initDB()
@@ -118,6 +154,7 @@ func main() {
 
 	router.GET("/projects", getPeople)
 	router.POST("/projects", postProject)
+	router.DELETE("/projects", deleteProject)
 	err := router.Run("localhost:8080")
 
 	if err != nil {
